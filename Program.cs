8ebeeb7380042df12f4ca43c6a38ab6869396889
F1lterEr <41ilter.er@gmail.com>
2023-04-07 11:13:18 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int x = 0;
+        string[] day = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+
+        Message message = (string[] array, ref int index) =>
+        {
+            if (index >= 6)
+            {
+                Console.WriteLine(array[index]);
+                index = 0;
+            }
+            else
+            {
+                Console.WriteLine(array[index]);
+                index += 1;
+            }
+        };
+
+        for (int i = 0; i < 16; i++)
+        {
+            message(day, ref x);
+        }
+    }
+    delegate void Message(string[] array, ref int index);
+}
